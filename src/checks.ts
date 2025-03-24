e02596221409df5d5da35e4aed9763d667e00566
Mark Boyd <mark.boyd@gsa.gov>
2025-03-24 14:25:22 -0400

    address TS lint issues

--- src/checks.ts
+++ src/checks.ts
@@ -1,36 +1,36 @@
-const scRegex = new RegExp('#+ security considerations', 'mi')
+const scRegex = new RegExp("#+ security considerations", "mi");
 
-function checkHeading(body: string): Boolean {
-  const checked = scRegex.test(body)
-  return checked
+function checkHeading(body: string): boolean {
+  const checked = scRegex.test(body);
+  return checked;
 }
 
-function checkDescription(body: string): Boolean {
-  const executed = scRegex.exec(body)
-  if (!executed) return false
+function checkDescription(body: string): boolean {
+  const executed = scRegex.exec(body);
+  if (!executed) { return false; }
 
-  const instance = executed[0]
-  const {index, input} = executed
-  const inputLength = input.trim().length
-  const startOfDescription = index + instance.length
+  const instance = executed[0];
+  const { index, input } = executed;
+  const inputLength = input.trim().length;
+  const startOfDescription = index + instance.length;
 
-  if (inputLength <= startOfDescription + 1) return false
+  if (inputLength <= startOfDescription + 1) { return false; }
 
-  return true
+  return true;
 }
 
-function checkTemplateNote(body: string): Boolean {
-  const regex = new RegExp('Note the any security considerations', 'mi')
-  const checked = regex.test(body)
-  return checked
+function checkTemplateNote(body: string): boolean {
+  const regex = new RegExp("Note the any security considerations", "mi");
+  const checked = regex.test(body);
+  return checked;
 }
 
-export function checkSecurityConsiderations(body: string): Boolean {
-  const hasHeading = checkHeading(body)
-  const hasDescription = checkDescription(body)
-  const hasTemplateNote = checkTemplateNote(body)
+export function checkSecurityConsiderations(body: string): boolean {
+  const hasHeading = checkHeading(body);
+  const hasDescription = checkDescription(body);
+  const hasTemplateNote = checkTemplateNote(body);
 
-  if (hasHeading && hasDescription && !hasTemplateNote) return true
+  if (hasHeading && hasDescription && !hasTemplateNote) { return true; }
 
-  return false
+  return false;
 }
