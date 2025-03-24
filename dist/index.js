416bee57fcd0a77df5e7b6e0c98a53e40e8354cc
Mark Boyd <mark.boyd@gsa.gov>
2025-03-24 15:22:04 -0400

    remove source map

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
+/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
 /***/ 7943:
@@ -31970,4 +31970,3 @@ module.exports = parseParams
 /******/ 	
 /******/ })()
 ;
-//# sourceMappingURL=index.js.map
