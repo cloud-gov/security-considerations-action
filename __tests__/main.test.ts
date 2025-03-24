6e5642dc72b16bc0236a4bc712988f9b43ea95cc
Mark Boyd <mark.boyd@gsa.gov>
2025-03-24 14:36:43 -0400

    apply TSlint fixes

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,58 +1,58 @@
-import {expect, test} from '@jest/globals'
-import {checkSecurityConsiderations} from '../src/checks'
+import {expect, test} from "@jest/globals";
+import {checkSecurityConsiderations} from "../src/checks";
 
-test('Successfully sets security considerations', async () => {
+test("Successfully sets security considerations", async () => {
   const input = `
   # Hello World
 
   ## Security Considerations
   - Hello
   - World
-  `
-  const checked = checkSecurityConsiderations(input)
-  await expect(checked).toBeTruthy()
-})
+  `;
+  const checked = checkSecurityConsiderations(input);
+  await expect(checked).toBeTruthy();
+});
 
-test('Successfully sets security considerations with one # heading', async () => {
+test("Successfully sets security considerations with one # heading", async () => {
   const input = `
   # Hello World
 
   # Security Considerations
   - Hello
   - World
-  `
-  const checked = checkSecurityConsiderations(input)
-  await expect(checked).toBeTruthy()
-})
+  `;
+  const checked = checkSecurityConsiderations(input);
+  await expect(checked).toBeTruthy();
+});
 
-test('Fails without security considerations heading', async () => {
+test("Fails without security considerations heading", async () => {
   const input = `
   # Hello World
 
   ## No Considerations
   None given
-  `
-  const checked = checkSecurityConsiderations(input)
-  await expect(checked).toBeFalsy()
-})
+  `;
+  const checked = checkSecurityConsiderations(input);
+  await expect(checked).toBeFalsy();
+});
 
-test('Fails without security considerations description', async () => {
+test("Fails without security considerations description", async () => {
   const input = `
   # Hello World
 
   ## Security Considerations
-  `
-  const checked = checkSecurityConsiderations(input)
-  await expect(checked).toBeFalsy()
-})
+  `;
+  const checked = checkSecurityConsiderations(input);
+  await expect(checked).toBeFalsy();
+});
 
-test('Fails when template note for security considerations not changed', async () => {
+test("Fails when template note for security considerations not changed", async () => {
   const input = `
   # Hello World
 
   ## Security Considerations
   [Note the any security considerations here, or make note of why there are none]
-  `
-  const checked = checkSecurityConsiderations(input)
-  await expect(checked).toBeFalsy()
-})
+  `;
+  const checked = checkSecurityConsiderations(input);
+  await expect(checked).toBeFalsy();
+});
